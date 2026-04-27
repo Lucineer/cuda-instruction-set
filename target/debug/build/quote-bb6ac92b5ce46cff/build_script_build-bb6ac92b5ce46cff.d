0d32ef52f605b740aecf87af459adea162cef65a
CedarBeach2019 <cedarbeach2019@users.noreply.github.com>
2026-04-26 16:34:30 -0800

    fix test syntax + add .gitignore

--- target/debug/build/quote-bb6ac92b5ce46cff/build_script_build-bb6ac92b5ce46cff.d
+++
@@ -1,5 +0,0 @@
-/tmp/cuda-instruction-set/target/debug/build/quote-bb6ac92b5ce46cff/build_script_build-bb6ac92b5ce46cff.d: /home/lucineer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/tmp/cuda-instruction-set/target/debug/build/quote-bb6ac92b5ce46cff/build_script_build-bb6ac92b5ce46cff: /home/lucineer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/lucineer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
--- target/debug/deps/cuda_instruction_set-22617c48124f9163.d
+++
@@ -1,5 +0,0 @@
-/tmp/cuda-instruction-set/target/debug/deps/cuda_instruction_set-22617c48124f9163.d: src/lib.rs
-
-/tmp/cuda-instruction-set/target/debug/deps/cuda_instruction_set-22617c48124f9163: src/lib.rs
-
-src/lib.rs:
